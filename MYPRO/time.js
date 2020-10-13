198a1eeaca2d525b253a8e0ddc31e0dceaddf224
amuge92 <67856951+amuge92@users.noreply.github.com>
2020-10-13 10:11:29 +0900

    new

--- MYPRO/time.js
+++ MYPRO/time.js
@@ -3,11 +3,10 @@ window.onload = function () {
     var t = time.toLocaleTimeString();
     var a = document.getElementById('time');
     a.innerHTML = t;
-   var v =  setInterval(function () {
-        var time = new Date;
-        var t = time.toLocaleTimeString();
-        var a = document.getElementById('time');
-        a.innerHTML = t;
-    }, 1000);
-    // clearInterval(v)
 }
+setInterval(function () {
+    var time = new Date;
+    var t = time.toLocaleTimeString();
+    var a = document.getElementById('time');
+    a.innerHTML = t;
+}, 1000);
