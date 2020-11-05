631b13c7681cb924c21fcdcfdbd58e0d2498c715
amuge92 <67856951+amuge92@users.noreply.github.com>
2020-11-05 16:32:48 +0900

    as

--- tp/src/co/soft/controller/GameController.java
+++ tp/src/co/soft/controller/GameController.java
@@ -9,51 +9,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/minigame")
 public class GameController {
-	
-	//@RequestMapping(value = "/minigame", method = RequestMethod.GET)
-	//public String minigame(HttpServletRequest request,HttpSession session) {
-	//	String check1 = request.getParameter("check1");
-	//	session.setAttribute("che1", check1);
-	//	
-	//	String check2 = request.getParameter("check2");
-	//	session.setAttribute("che2", check2);
-	//	
-	//	
-	//	return "Minigame/maingame";
-	//}
 	@RequestMapping(value = "/minigame2", method = RequestMethod.GET)
 	public String minigame2(HttpServletRequest request,HttpSession session) {
-		
-		
 		String check1 = request.getParameter("check1");
 		session.setAttribute("che1", check1);
-		//mo.addAttribute("che1",check1);
-		System.out.println(check1);
 		
 		String check2 = request.getParameter("check2");
-		//mo.addAttribute("che2",check2);
 		session.setAttribute("che2", check2);
-		System.out.println(check2);
 		
 		return "minigame2/minigame2";
 	}
 	
 	@RequestMapping(value = "/minigame1", method = RequestMethod.GET)
 	public String minigame1(HttpServletRequest request,HttpSession session) {
-		
 		String check1 = request.getParameter("check1");
 		session.setAttribute("che1", check1);
-		//mo.addAttribute("che1",check1);
-		System.out.println(check1);
 		
 		String check2 = request.getParameter("check2");
-		//mo.addAttribute("che2",check2);
 		session.setAttribute("che2", check2);
-		System.out.println(check2);
-		
-		
-		
 		return "minigame2/minigame1";
 	}
 	
--- tp/src/co/soft/controller/HomeController.java
+++ tp/src/co/soft/controller/HomeController.java
@@ -20,22 +20,6 @@ public class HomeController {
 		return "index";
 	}
 
-	@RequestMapping(value = "/index", method = RequestMethod.GET)
-	public String gameok(HttpServletRequest request, Model mo, HttpSession session) {
-		
-		if (request.getParameter("clear").equals("true")) {
-			mo.addAttribute("clear", "t");
-		}
-		
-		String che1 = (String) session.getAttribute("che1");
-        mo.addAttribute("che1",che1);
-        System.out.println(che1);
-        
-        String che2 = (String) session.getAttribute("che2");
-        mo.addAttribute("che2",che2);
-        System.out.println(che2);
-        
-		return "index";
-	}
+	
 
 }
